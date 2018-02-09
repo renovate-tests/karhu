d8f32ec59235d97626b33dcdfb6e8a7e42598edb
Tapani Haka <tapani.haka@barona.fi>
2018-02-09 15:05:13 +0200

    Add requestpromise StatusCodeError support to jelpp config

--- src/config/jelpp.ts
+++ src/config/jelpp.ts
@@ -9,7 +9,8 @@ const jelppConfig: KarhuConfig = {
     ...defaultConfig.colors,
     NOTICE: ansiColor.blueBright
   },
-  logLevels: updatedLogLevels()
+  logLevels: updatedLogLevels(),
+  outputMapper: requestPromiseOutputMapper
 }
 
 export default jelppConfig
@@ -18,4 +19,12 @@ function updatedLogLevels() {
   const ll = [...defaultConfig.logLevels]
   ll.splice(ll.indexOf('WARN'), 0, 'NOTICE')
   return ll
+}
+
+function requestPromiseOutputMapper(val: any) {
+  if (val instanceof Error && val.name === 'StatusCodeError') {
+    const err = val as any
+    return `StatusCodeError: ${err.statusCode} ${val.message}\n\n${err.response && err.response.body}\n\n${err.stack}`
+  }
+  return val
 }
